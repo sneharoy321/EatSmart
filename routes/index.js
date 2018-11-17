70fd6cd5bdcd0088e07d0f77d44410e5575a0ff4
sneharoy321 <31810043+sneharoy321@users.noreply.github.com>
2018-11-17 10:55:19 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EatSmart' });
+  res.render('index', { title: 'EatSmart App' });
 });
 
 module.exports = router;
